fef8229cce27e4243c37393998033f9330735468
MINTIN17 <hmtintk@gmail.com>
2024-06-03 23:50:35 +0700

    update

--- run.py
+++ run.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     # socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
     # eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
     # socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+    # app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+    app.run(debug=True)
     # socketio.run(app)
 
